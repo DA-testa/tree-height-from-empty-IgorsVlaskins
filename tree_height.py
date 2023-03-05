240d1fa5b974d015dbddd4531e4a9447e8c4c4fd
IgorsVlaskins <125510437+IgorsVlaskins@users.noreply.github.com>
2023-03-05 22:19:13 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,7 +4,7 @@ import threading
 
 def computeheight(r, parents):
     # izveido koku
-    children = [[] for / in range(r)]
+    children = [[] for p in range(r)]
     for i in range(r):
         parent = parents[p]
         if parent == -1:
